5fd7021b9b75bc0dcae61be634e305e48fe11f2c
zeevo <8338013+zeevosec@users.noreply.github.com>
2018-11-28 20:33:10 -0500

    Add tests

--- api/app/tests.py
+++ api/app/tests.py
@@ -1,3 +1,10 @@
 from django.test import TestCase
+from rest_framework.test import APIClient
 
-# Create your tests here.
+
+class HelloWorldViewTest(TestCase):
+
+    def test_hello_world_view(self):
+        client = APIClient()
+        response = client.get('/api/title')
+        self.assertEqual({'title': 'Hello World'}, response.json())
